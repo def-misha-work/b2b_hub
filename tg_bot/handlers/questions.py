8c00b8356b254df18ed95912139fd41e8543d85f
BaronFAS <mikhail-upside@yandex.ru>
2024-03-28 17:18:44 +0300

    Go to test

--- tg_bot/handlers/questions.py
+++ tg_bot/handlers/questions.py
@@ -1,4 +1,5 @@
 ﻿import logging
+import json
 
 from aiogram import Router, F, types
 from aiogram.filters import Command, StateFilter
@@ -13,7 +14,7 @@ from storage import UserStorage, ApplicationStorage
 from utils import send_message
 from validators import validate_date
 from constants import (
-    MESSAGES, ENDPONT_CREATE_USER, SERVICE_CHAT_ID, ENDPONT_CREATE_APPLICATION, MESSAGES_TO_MANAGER, ENDPONT_GET_APPLICATION_LIST,
+    MESSAGES, TECH_MESSAGES, ENDPONT_CREATE_USER, SERVICE_CHAT_ID, ENDPONT_CREATE_APPLICATION, MESSAGES_TO_MANAGER, ENDPONT_GET_APPLICATION_LIST,
 )
 
 
@@ -159,22 +160,31 @@ async def get_target_date(message: types.Message, state: FSMContext):
     logging.info("Успех шаг 4")
 
     application_dict = application_storage.to_dict()
+    application_id = False
     try:
         response = await make_post_request(
             ENDPONT_CREATE_APPLICATION, application_dict
         )
         if response.status_code != 201:
-            logging.info(f"Заявка не создана БД: {response.status_code}")
-            await send_message(SERVICE_CHAT_ID, "Заявка не создана БД")
-        # logging.info(f"Это ответ на POST: {response.text}")
+            logging.info(
+                f"Заявка не создана получен ответ: {response.status_code}"
+            )
+            await send_message(
+                SERVICE_CHAT_ID,
+                f"Заявка не создана получен ответ: {response.status_code}"
+            )
+        data = json.loads(response.text)
+        # logging.info(f"Это ответ POST: {data}")
+        application_id = data["application_id"]
         logging.info("Заявка создана в БД")
     except Exception as e:
         logging.info(f"Ошибка при создании заявки: {e}")
         await send_message(SERVICE_CHAT_ID, "Ошибка создания заявки")
-    
+        await message.answer(TECH_MESSAGES["api_error"])
+
     # Отправляем в саппорт
-        application_id = response.text["application_id"]
-        application_storage.application_id(application_id)
+    if application_id:
+        application_storage.update_application_id(application_id)
         application_info = MESSAGES["application"].format(
             application_storage.application_id,
             application_storage.inn_payer,
@@ -192,11 +202,11 @@ async def get_target_date(message: types.Message, state: FSMContext):
         # Отправляем менеджеру
         # await send_message(MANAGER_CHAT_ID, application_to_manager) TODO Расскоментить на бою. # noqa
         # logging.info("Заявка отправлена менеджеру")
-    await message.answer("Ваша заявка:" + application_info)
-    await message.answer(MESSAGES["application_created"])
-    await state.set_state(None)
-    await message.answer(MESSAGES["menu"], reply_markup=get_menu())
-    logging.info("Пользователь в меню")
+        await message.answer("Ваша заявка:" + application_info)
+        await message.answer(MESSAGES["application_created"])
+        await state.set_state(None)
+        await message.answer(MESSAGES["menu"], reply_markup=get_menu())
+        logging.info("Пользователь в меню")
 
 
 @router.message(F.text, NewApplication.step_4)
@@ -212,14 +222,44 @@ async def invalid_values_target_date(
 
 
 @router.message(F.text.lower() == "мои заявки")
-async def answer_no(message: Message):
-    """Обрабатывает клик по кнопке."""
-    tg_id = message.from_user.id
-    response = await make_get_request(ENDPONT_GET_APPLICATION_LIST, tg_id)
-    print(response.status_code)
-    print(response.text)
-    await message.answer("Ваши заявки: " + response.text)
+async def get_application_list(message: Message):
+    """Обрабатывает клик по кнопке Список заявок."""
     logging.info("Пользователь запросил заявки")
+    tg_id = message.from_user.id
+    application_list = False
+    try:
+        response = await make_get_request(ENDPONT_GET_APPLICATION_LIST, tg_id)
+        if response.status_code != 201:
+            logging.info(
+                f"Список заявок не получен ответ: {response.status_code}"
+            )
+            await send_message(
+                SERVICE_CHAT_ID,
+                f"Список заявок не получен ответ: {response.status_code}"
+            )
+        application_list = json.loads(response.text)
+        logging.info("Ответ от БД получен")
+    except Exception as e:
+        logging.info(f"Ошибка при получение спика заявок: {e}")
+        await send_message(
+            SERVICE_CHAT_ID, f"Ошибка при получение спика заявок: {e}"
+        )
+        await message.answer(TECH_MESSAGES["api_error"])
+
+    if application_list:
+        for application in application_list:
+            answer = MESSAGES["application"].format(
+                application["id"],
+                *application["inn_payer"],
+                *application["inn_recipient"],
+                application["cost"],
+                application["target_date"],
+            )
+            await message.answer(f"Ваши заявки: {answer}")
+        logging.info("Пользователь получил список заявок")
+    else:
+        await message.answer("У вас нет активных заявок.")
+        logging.info("Пользователь получил список заявок (пустой)")
 
 
 @router.message(F.text.lower() == "мои юр. лица")
@@ -227,5 +267,6 @@ async def answer_no1(message: Message):
     """Обрабатывает клик по кнопке."""
     tg_id = message.from_user.id
     response = await make_get_request(ENDPONT_GET_APPLICATION_LIST, tg_id)
-    await message.answer("Ваши юр. лица: " + response.text) # TODO Получить по api название компании
+    logging.info(response)
+    await message.answer("Пока не работает!") # TODO Получить по api название компании
     logging.info("Пользователь запросил юр. лица")
